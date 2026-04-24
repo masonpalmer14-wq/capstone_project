18ebb583ab3740953aad7fa97448bc390d4d2065
snyderp1 <snyderp@etown.edu>
2026-04-23 21:13:51 -0400

    main.py countries changed, cleaning adapted, succcesully got csvs to datagrip

---
+++ cleaning/cleaning_test.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+#reading new cleaned files in to check discrepency in row counts that wasnt there before 
+#   european union was added and germany was taken out. 
+service_m_clean = pd.read_csv("test_cleaned/service_m_clean.csv")
+merch_m_clean = pd.read_csv("test_cleaned/merch_m_clean.csv")
+
+#check_df.info()
+
+#print(service_m_clean.isnull().sum())  # found no nulls in any of the 5 csvs 
+#print(service_m_clean.head())
+#print(merch_m_clean.head())
+
+#finding difference in csvs
+service_keys = set(zip(service_m_clean["Country"], service_m_clean["Year"]))
+merch_keys   = set(zip(merch_m_clean["Country"], merch_m_clean["Year"]))
+
+extra_in_merch = merch_keys - service_keys
+
+print(extra_in_merch)
+print(len(extra_in_merch))  
+
+# found four less rows: accurate based on the count i see in the csvs themselves
+
+#locating rows that are additional 
+extra_rows = merch_m_clean[
+    merch_m_clean.set_index(["Country", "Year"]).index.isin(extra_in_merch)
+]
+
+print(extra_rows)
